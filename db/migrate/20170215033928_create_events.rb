c9def7fdfb8b903491ab98c3ce4753207652ae7d
dejo-olakareem <oladejoolakareem@yahoo.com>
2017-02-14 19:49:56 -0800

    Add attendance model

--- db/migrate/20170215033928_create_events.rb
+++ db/migrate/20170215033928_create_events.rb
@@ -1,6 +1,12 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
+    	t.date :start_date
+    	t.time :start_time
+    	t.date :end_date
+    	t.time :end_time
+    	t.string :location
+
 
       t.timestamps
     end
